8c08f71806f2bd7f8b8445c5c9a0327536a4fce6
Ethan Pursley <ethan.k.pursley@gmail.com>
2015-03-22 21:48:37 -0700

    Exercise 2 Answer Code

--- test/e2e/login.spec.js
+++ test/e2e/login.spec.js
@@ -9,13 +9,21 @@ describe('Login Submission', function () {
 
     describe('Authentication Attempt', function () {
         it('logs in when given the correct credentials', function () {
+            page.fillEmail();
+            page.fillPassword();
+            page.submitBtn.click();
 
+            expect(browser.getLocationAbsUrl()).toBe('/landing');
         });
     });
 
     describe('Bad Authentication Attempt', function () {
         it('displays an error when given incorrect credentials', function () {
+            page.fillEmail();
+            page.passwordInput.sendKeys('incorrectPassword');
+            page.submitBtn.click();
 
+            expect(page.errorContainer.isPresent()).toBe(true);
         });
     });
 })
