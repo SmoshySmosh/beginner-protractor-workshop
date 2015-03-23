1ebfa82b3098905e01824df1c48bf31c1d85591a
Ethan Pursley <ethan.k.pursley@gmail.com>
2015-03-22 21:13:35 -0700

    Exercise 1 Answer Code

--- test/e2e/index.spec.js
+++ test/e2e/index.spec.js
@@ -12,5 +12,15 @@ describe('Login Form', function () {
             var forgotLink = element(by.linkText('Forgot your password?'));
             expect(forgotLink.isPresent()).toBe(true);
         });
+
+        it('should display the expected submit button text', function () {
+            var submitBtn = element(by.buttonText('Log in'));
+            expect(submitBtn.isPresent()).toBe(true);
+        });
+
+        it('should go to /forgot when clicking the forgot link', function () {
+            element(by.linkText('Forgot your password?')).click();
+            expect(browser.getLocationAbsUrl()).toBe('/forgot');
+        });
     });
 });
